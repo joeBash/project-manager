734279e6d1e7ac4591de2cd924cae382839f620a
Joe Bashour <joebashour1997@gmail.com>
2025-03-08 18:32:45 -0500

    [TEMP] Rework routing
    Simple hotfix to get routes to work until authentication issues are resolved.
    Will now utilize session-based (browser) authentication and not OAuth tokens
    as implemented by Passport.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AttributeController;
+use App\Http\Controllers\AttributeValueController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TimesheetController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -9,6 +13,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::prefix('/api')->group(function () {
+        Route::resource('projects', ProjectController::class);
+        Route::resource('timesheets', TimesheetController::class);
+        Route::resource('attributes', AttributeController::class);
+        Route::resource('attribute-values', AttributeValueController::class);
+    });
+
 });
 
 require __DIR__ . '/settings.php';
